632a71062371c2bbe121f6c73cd98885d08ac215
Lisa W <wan.lis@northeastern.edu>
2025-08-14 01:27:55 -0400

    Fixed Some Formatting

--- src/app/components/ContactInfo.tsx
+++ src/app/components/ContactInfo.tsx
@@ -1,17 +1,15 @@
 export default function ContactInfo() {
     return (
     <footer>
-      <section className="footer">
       <div className="contact-info">
         <h3>Contact Me</h3>
-        <h2> 
+        <p> 
           Email: <a href="mailto:wan.lis@northeastern.edu">wan.lis@northeastern.edu</a><br />
           Discord: justalittlemushroominthecorner<br />
           GitHub: <a href="https://github.com/justalittlemushroom" target="_blank" rel="noopener noreferrer">github.com/justalittlemushroom</a><br />
           LinkedIn: <a href="https://linkedin.com/in/lisa-wan" target="_blank" rel="noopener noreferrer">linkedin.com/in/lisa-wan</a><br />
-        </h2>
+        </p>
       </div>
-      </section>
     </footer>
   )
 }
