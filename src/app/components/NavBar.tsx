be89da161ed59633629255329fc28b4c6770c62d
Lisa W <wan.lis@northeastern.edu>
2025-08-14 23:12:53 -0400

    Fixed Minor Formatting Issues

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -11,42 +11,44 @@ export default function NavBar() {
 
 
   return (
-    <nav className="nav-bar">
-        <div className="corner-ornament top-right">
-            <Image 
-                src="/victorian-ornamental-corner.PNG" 
-                alt="" 
-                width={300}
-                height={150}/>
-        </div>
-        <div className="corner-ornament bottom-left">
-            <Image 
-                src="/victorian-ornamental-corner.PNG" 
-                alt="" 
-                width={300}
-                height={150}/>
-        </div>
-        <Link href="/" className="home-button"> <Home size={homeIconSize} /> </Link>
-        <div className="nav-section">
-            <header className="nav-section-header">Personal Journey</header>
-            <Link href="/about-me" className="nav-link">About Me</Link>
-            <Link href="/lexical-constructs" className="nav-link">Lexical Constructs</Link>
-        </div>
-        <div className="nav-section">
-            <header className="nav-section-header">Intellectual Work</header>
-            <Link href="/research" className="nav-link">Research</Link>
-            <Link href="/curiosity-cabinet" className="nav-link">Curiosity Cabinet</Link>
-        </div>
-        <div className="nav-section">
-            <header className="nav-section-header">Cultural Work</header>
-            <Link href="/travel-log" className="nav-link">Travel Log</Link>
-            <Link href="/creative-modalities" className="nav-link">Creative Modalities</Link>
-        </div>
-        <div className="nav-section">
-            <header className="nav-section-header">Meta</header>
-            <Link href="/behind-the-blog" className="nav-link">Behind the Blog</Link>
-            <Link href="/archives" className="nav-link">Archives</Link>
-        </div>
-    </nav>
+    <div className="nav-container">
+        <nav className="nav-bar">
+            <div className="corner-ornament top-right">
+                <Image 
+                    src="/victorian-ornamental-corner.PNG" 
+                    alt="" 
+                    width={300}
+                    height={150}/>
+            </div>
+            <div className="corner-ornament bottom-left">
+                <Image 
+                    src="/victorian-ornamental-corner.PNG" 
+                    alt="" 
+                    width={300}
+                    height={150}/>
+            </div>
+            <Link href="/" className="home-button"> <Home size={homeIconSize} /> </Link>
+            <div className="nav-section">
+                <header className="nav-section-header">Personal Journey</header>
+                <Link href="/about-me" className="nav-link">About Me</Link>
+                <Link href="/lexical-constructs" className="nav-link">Lexical Constructs</Link>
+            </div>
+            <div className="nav-section">
+                <header className="nav-section-header">Intellectual Work</header>
+                <Link href="/research" className="nav-link">Research</Link>
+                <Link href="/curiosity-cabinet" className="nav-link">Curiosity Cabinet</Link>
+            </div>
+            <div className="nav-section">
+                <header className="nav-section-header">Cultural Work</header>
+                <Link href="/travel-log" className="nav-link">Travel Log</Link>
+                <Link href="/creative-modalities" className="nav-link">Creative Modalities</Link>
+            </div>
+            <div className="nav-section">
+                <header className="nav-section-header">Meta</header>
+                <Link href="/behind-the-blog" className="nav-link">Behind the Blog</Link>
+                <Link href="/archives" className="nav-link">Archives</Link>
+            </div>
+        </nav>
+    </div>
   )
 }
--- src/app/hooks/useDeviceType.js
+++ src/app/hooks/useDeviceType.js
@@ -6,11 +6,13 @@ function useDeviceType() {
   const [isMobile, setIsMobile] = useState(false);
   const [isTablet, setIsTablet] = useState(false);
 
+  const [isLoaded, setIsLoaded] = useState(false)
+
   useEffect(() => {
     const checkMobile = () => {
-
         setIsMobile(window.innerWidth <= 768)
         setIsTablet(window.innerWidth > 768 && window.innerWidth <= 1024)
+        setIsLoaded(true)
     }
     checkMobile()
     window.addEventListener('resize', checkMobile)
@@ -19,7 +21,7 @@ function useDeviceType() {
 
   const isDesktop = !isMobile && !isTablet;
 
-  return {isMobile, isTablet, isDesktop};
+  return {isMobile, isTablet, isDesktop, isLoaded};
 }
 
 export default useDeviceType;
